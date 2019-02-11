3c330a6dd6f8bb01e2dadec5fc391ba68de5e4df
danie <danie@DESKTOP-CO9ODIP>
2019-02-11 20:21:25 +0000

    Add user input ID to board.

--- src/ie/gmit/game/Main.java
+++ src/ie/gmit/game/Main.java
@@ -17,12 +17,25 @@ public class Main {
 		
 		System.out.println("Welcome to Tic-Tac-Toe");
 		
+		// Print Board
+		for (int i = 0; i < board.length; i++) {
+			System.out.print("\t"); // Add tab to shift board display right.
+			System.out.println(board[i]);
+		}
+		
 		System.out.println("Select a row (1 - 3): ");
 		row = userInput.nextInt();
 		System.out.println("Select a column (1 - 3): ");
 		column = userInput.nextInt();
-
-		System.out.println("Row: " + row + " Column: " + column);
+		
+		// Assign user ID (i.e. X or O) to board location (i.e. (row, column))
+		board[row - 1][column - 1] = 'X';
+		
+		// Print Board
+		for (int i = 0; i < board.length; i++) {
+			System.out.print("\t"); // Add tab to shift board display right.
+			System.out.println(board[i]);
+		}
 		
 		userInput.close(); // Close Scanner object.
 		
