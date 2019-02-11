50f4019101f51c05343daee0e2ae34b97a3254d7
danie <danie@DESKTOP-CO9ODIP>
2019-02-11 20:10:15 +0000

    Created board structure. Added row and column variables.

--- src/ie/gmit/game/Main.java
+++ src/ie/gmit/game/Main.java
@@ -7,14 +7,24 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scanner userInput = new Scanner(System.in);
-
+		// Create Tic-Tac-Toe Board
+						 // col1,col2,col3
+		char[][] board = { { '?', '?', '?' },   // row 1
+						   { '?', '?', '?' },   // row 2
+						   { '?', '?', '?' } }; // row 3
+		int row = 0;
+		int column = 0;
+		
 		System.out.println("Welcome to Tic-Tac-Toe");
 		
-		System.out.println("Please enter a number: ");
-		int x = userInput.nextInt();
-		System.out.println(x);
+		System.out.println("Select a row (1 - 3): ");
+		row = userInput.nextInt();
+		System.out.println("Select a column (1 - 3): ");
+		column = userInput.nextInt();
 
-		userInput.close();
+		System.out.println("Row: " + row + " Column: " + column);
+		
+		userInput.close(); // Close Scanner object.
 		
 	} // End Main Method
 
