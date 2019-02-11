5d2bbabd2f69d7d8fa61710aad2fcaffa3b7a0f6
danie <danie@DESKTOP-CO9ODIP>
2019-02-11 19:59:33 +0000

    Import Scanner class and test

--- src/ie/gmit/game/Main.java
+++ src/ie/gmit/game/Main.java
@@ -1,9 +1,21 @@
 package ie.gmit.game;
 
+import java.util.Scanner; // Adding to take user input
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Tic-Tac-Toe");
-    }
-    
-}
+	public static void main(String[] args) {
+		
+		Scanner userInput = new Scanner(System.in);
+
+		System.out.println("Welcome to Tic-Tac-Toe");
+		
+		System.out.println("Please enter a number: ");
+		int x = userInput.nextInt();
+		System.out.println(x);
+
+		userInput.close();
+		
+	} // End Main Method
+
+} // End Main Class
