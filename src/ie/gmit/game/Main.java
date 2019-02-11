ff53b848810b13a73f1cd18921f503b2b9303129
danie <danie@DESKTOP-CO9ODIP>
2019-02-11 20:42:33 +0000

    Move display board into its own method and update.

--- src/ie/gmit/game/Main.java
+++ src/ie/gmit/game/Main.java
@@ -3,25 +3,21 @@ package ie.gmit.game;
 import java.util.Scanner; // Adding to take user input
 
 public class Main {
-
+	
+	// Create Tic-Tac-Toe Board
+					        // col1,col2,col3
+	static char[][] board = { { '?', '?', '?' },   // row 1
+					          { '?', '?', '?' },   // row 2
+					          { '?', '?', '?' } }; // row 3
+	
 	public static void main(String[] args) {
-		
 		Scanner userInput = new Scanner(System.in);
-		// Create Tic-Tac-Toe Board
-						 // col1,col2,col3
-		char[][] board = { { '?', '?', '?' },   // row 1
-						   { '?', '?', '?' },   // row 2
-						   { '?', '?', '?' } }; // row 3
 		int row = 0;
 		int column = 0;
 		
 		System.out.println("Welcome to Tic-Tac-Toe");
 		
-		// Print Board
-		for (int i = 0; i < board.length; i++) {
-			System.out.print("\t"); // Add tab to shift board display right.
-			System.out.println(board[i]);
-		}
+		displayBoard();
 		
 		System.out.println("Select a row (1 - 3): ");
 		row = userInput.nextInt();
@@ -31,14 +27,19 @@ public class Main {
 		// Assign user ID (i.e. X or O) to board location (i.e. (row, column))
 		board[row - 1][column - 1] = 'X';
 		
-		// Print Board
-		for (int i = 0; i < board.length; i++) {
-			System.out.print("\t"); // Add tab to shift board display right.
-			System.out.println(board[i]);
-		}
+		displayBoard();
 		
 		userInput.close(); // Close Scanner object.
 		
 	} // End Main Method
 
+	static void displayBoard() {
+		System.out.println(" -----------");
+		System.out.println("| " + board[0][0] + " | " + board[0][1] + " | " + board[0][2] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[1][0] + " | " + board[1][1] + " | " + board[1][2] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[2][0] + " | " + board[2][1] + " | " + board[2][2] + " |");
+		System.out.println(" -----------");
+	}
 } // End Main Class
